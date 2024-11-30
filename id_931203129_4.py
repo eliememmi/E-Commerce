582378d01e906fa39927f1805087c4277789fecc
eliememmi <82410811+eliememmi@users.noreply.github.com>
2024-11-30 22:37:54 +0200

    Add files via upload

---
+++ id_931203129_4.py
@@ -0,0 +1,26 @@
+class BiddingAgent1:
+    def __init__(self):
+        pass
+
+    def get_bid(self, num_of_agents, P, q, v):
+
+        return 0.435 * v * 0.7 + 1
+
+    def notify_outcome(self, reward, outcome, position):
+        pass
+
+    def get_id(self):
+        return "id_931203129_932191265"
+
+class BiddingAgent2:
+    def __init__(self):
+        pass
+
+    def get_bid(self, num_of_agents, P, q, v):
+        return 0.435 * v * 0.7 + 1
+
+    def notify_outcome(self, reward, outcome, position):
+        pass
+
+    def get_id(self):
+        return "id_931203129_932191265"
